e4e3d584da84abbb2c6b322bc6a5ffe873cf3232
Calil_Silva <ka.renner@hotmail.com>
2021-07-23 00:19:10 -0300

    Já é possível selecionar um item de cada categoria

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Pratos início
 document.getElementById("bordas").addEventListener("click", selecionarAtivo);
 document.getElementById("bordas2").addEventListener("click", selecionarAtivo2);
 document.getElementById("bordas3").addEventListener("click", selecionarAtivo3);
@@ -5,21 +6,204 @@ document.getElementById("bordas4").addEventListener("click", selecionarAtivo4);
 document.getElementById("bordas5").addEventListener("click", selecionarAtivo5);
 
 function selecionarAtivo() {
-    document.getElementById("bordas").style.border = "5px solid rgba(50, 183, 47, 1)";
+    document.getElementById("bordas").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
 }
-function selecionarAtivo2 () {
-    document.getElementById("bordas2").style.border = "5px solid rgba(50, 183, 47, 1)";
+function selecionarAtivo2() {
+    document.getElementById("bordas2").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo3() {
+    document.getElementById("bordas3").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo4() {
+    document.getElementById("bordas4").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo5() {
+    document.getElementById("bordas5").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
 }
 
 
 function teste() {
-if(selecionarAtivo2) {
-    document.getElementById("bordas").style.border = "none";
+    if (selecionarAtivo) {
+        document.getElementById("bordas2").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas3").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas4").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas5").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
 }
-} 
 
 function teste2() {
-    if(selecionarAtivo) {
-        document.getElementById("bordas2").style.border = "none";
-    } 
+    if (selecionarAtivo2) {
+        document.getElementById("bordas").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas3").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas4").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas5").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
 }
+
+function teste3() {
+    if (selecionarAtivo3) {
+        document.getElementById("bordas").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas2").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas4").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas5").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste4() {
+    if (selecionarAtivo4) {
+        document.getElementById("bordas").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas2").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas3").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas5").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste5() {
+    if (selecionarAtivo5) {
+        document.getElementById("bordas").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas2").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas3").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas4").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+//Pratos final
+
+//Bebidas início
+document.getElementById("bordas6").addEventListener("click", selecionarAtivo6);
+document.getElementById("bordas7").addEventListener("click", selecionarAtivo7);
+document.getElementById("bordas8").addEventListener("click", selecionarAtivo8);
+document.getElementById("bordas9").addEventListener("click", selecionarAtivo9);
+document.getElementById("bordas10").addEventListener("click", selecionarAtivo10);
+
+function selecionarAtivo6() {
+    document.getElementById("bordas6").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo7() {
+    document.getElementById("bordas7").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo8() {
+    document.getElementById("bordas8").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo9() {
+    document.getElementById("bordas9").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo10() {
+    document.getElementById("bordas10").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+
+
+function teste6() {
+    if (selecionarAtivo6) {
+        document.getElementById("bordas7").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas8").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas9").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas10").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste7() {
+    if (selecionarAtivo7) {
+        document.getElementById("bordas6").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas8").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas9").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas10").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste8() {
+    if (selecionarAtivo8) {
+        document.getElementById("bordas6").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas7").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas9").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas10").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste9() {
+    if (selecionarAtivo9) {
+        document.getElementById("bordas6").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas7").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas8").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas10").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste10() {
+    if (selecionarAtivo10) {
+        document.getElementById("bordas6").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas7").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas8").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas9").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+//Bebidas final
+
+//Sobremesas início
+document.getElementById("bordas11").addEventListener("click", selecionarAtivo11);
+document.getElementById("bordas12").addEventListener("click", selecionarAtivo12);
+document.getElementById("bordas13").addEventListener("click", selecionarAtivo13);
+document.getElementById("bordas14").addEventListener("click", selecionarAtivo14);
+document.getElementById("bordas15").addEventListener("click", selecionarAtivo15);
+
+function selecionarAtivo11() {
+    document.getElementById("bordas11").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo12() {
+    document.getElementById("bordas12").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo13() {
+    document.getElementById("bordas13").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo14() {
+    document.getElementById("bordas14").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+function selecionarAtivo15() {
+    document.getElementById("bordas15").style.boxShadow = "0 0 0 5px rgba(50, 183, 47, 1) inset";
+}
+
+
+function teste11() {
+    if (selecionarAtivo11) {
+        document.getElementById("bordas12").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas13").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas14").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas15").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste12() {
+    if (selecionarAtivo12) {
+        document.getElementById("bordas11").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas13").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas14").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas15").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste13() {
+    if (selecionarAtivo13) {
+        document.getElementById("bordas11").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas12").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas14").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas15").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste14() {
+    if (selecionarAtivo14) {
+        document.getElementById("bordas11").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas12").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas13").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas15").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+
+function teste15() {
+    if (selecionarAtivo15) {
+        document.getElementById("bordas11").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas12").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas13").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+        document.getElementById("bordas14").style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
+    }
+}
+//Sobremesas final
